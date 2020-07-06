06cf748f378c8c4d9e1b0161bc8c68732cfe8780
Kristian Portillo <portillk@gmail.com>
2020-07-06 18:33:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,10 @@
 # Write your methods here
-def loop_message_five_times
-"Hello World.\n"
+def loop_message_five_times(message)
+message = "Hello World.\n"
+count = 0
+while count < 5
+puts message
+count +=1
 end
 
 def loop_message_n_times
